f88a272f8d1cce5a22aafdbdb795a2f839a021d7
Your Name <you@example.com>
2021-09-07 20:53:55 +0200

    fixed v_layout, h_layout

--- src/gui_orchestrator.rs
+++ src/gui_orchestrator.rs
@@ -13,21 +13,21 @@ pub enum Node {
     Leaf(Tile),
 }
 /// [] [] []
-pub fn h_layout(elements: [Node]) -> Node {
-    match elements {
-        Node::Int(laouts) => Node::Int(laouts),
-        Node::Leaf(leaf) => Node::Leaf(leaf),
-    }
+pub fn h_layout<T>(elements: T) -> Node where T : IntoIterator<Item = Node> {
+    Node::Int(elements.into_iter().map(|e| Box::new(e)).collect())
 }
 
 /// []
 /// []
 /// []
-pub fn v_layout(elements: [Node]) -> Node {
-    match elements {
-        Node::Int(laouts) => Node::Int(laouts),
-        Node::Leaf(leaf) => Node::Leaf(leaf),
-    }
+/// T, where T : IntoIterator<Item = Node>
+/// Meaning any kind of collection, which implements IntoIterator, where returning
+/// value is Node.
+/// That includes arrays - of any size!
+pub fn v_layout<T>(elements: T) -> Node where T : IntoIterator<Item = Node> {
+    // Here real magic begins, because possibly stack allocated element
+    // becomes heap allocated
+    Node::Int(elements.into_iter().map(|e| Box::new(e)).collect())
 }
 
 #[cfg(test)]
@@ -36,43 +36,49 @@ mod test {
     #[test]
     fn sample_gui() {
         let gui = v_layout([
-            Box::new(Node::Leaf(Tile {
+            Node::Leaf(Tile {
                 name: "text",
-                ..Tile::default()
-            })),
+                // Some type magic inference?
+                ..Default::default()
+            }),
+            Node::Leaf(Tile {
+                name: "text",
+                // Some type magic inference?
+                ..Default::default()
+            }),
         ]);
 
-        let gui = Node::Int(vec![
-            // vert layout
-            Box::new(Node::Int(vec![
-                // h layout
-                Box::new(Node::Int(vec![
-                    // v layout
-                    Box::new(Node::Leaf(Tile {
-                        name: "text",
-                        ..Tile::default()
-                    })),
-                    Box::new(Node::Leaf(Tile {
-                        name: "%d",
-                        ..Tile::default()
-                    })),
-                    Box::new(Node::Int(vec![
-                        // v layout
-                        Box::new(Node::Leaf(Tile {
-                            name: "text",
-                            ..Tile::default()
-                        })),
-                        Box::new(Node::Leaf(Tile {
-                            name: "%d",
-                            ..Tile::default()
-                        })),
-                    ])),
-                ])),
-            ])),
-            Box::new(Node::Int(vec![ // h layout
+        // let gui = Node::Int(vec![
+        //     // vert layout
+        //     Box::new(Node::Int(vec![
+        //         // h layout
+        //         Box::new(Node::Int(vec![
+        //             // v layout
+        //             Box::new(Node::Leaf(Tile {
+        //                 name: "text",
+        //                 ..Tile::default()
+        //             })),
+        //             Box::new(Node::Leaf(Tile {
+        //                 name: "%d",
+        //                 ..Tile::default()
+        //             })),
+        //             Box::new(Node::Int(vec![
+        //                 // v layout
+        //                 Box::new(Node::Leaf(Tile {
+        //                     name: "text",
+        //                     ..Tile::default()
+        //                 })),
+        //                 Box::new(Node::Leaf(Tile {
+        //                     name: "%d",
+        //                     ..Tile::default()
+        //                 })),
+        //             ])),
+        //         ])),
+        //     ])),
+        //     Box::new(Node::Int(vec![ // h layout
 
-            ])),
-        ]);
+        //     ])),
+        // ]);
 
         println!("gui: {:#?}", gui);
     }
